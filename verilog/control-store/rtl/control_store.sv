b6baa5485ca24a3fcc57375bf79a3322f22e2d0f
unknown <zwitte46@gmail.com>
2021-10-21 19:28:06 +0200

    added rtl

--- verilog/control-store/rtl/control_store.sv
+++ verilog/control-store/rtl/control_store.sv
@@ -1,5 +1,18 @@
 `timescale 1 ns / 1 ps
 
 module control_store (
-    input [8:0] addr,
-    input [
+    input clk, wen, ren,
+
+    input [8:0] waddr, raddr
+    input [31:0] wdata,
+    output reg [31:0] rdata
+    );
+
+    reg [31:0] mem [0:511];
+    always @(posedge clk) begin
+         if (wen)
+            mem[waddr] <= wdata;
+         if (ren)
+            rdata <= mem[raddr]
+
+endmodule
