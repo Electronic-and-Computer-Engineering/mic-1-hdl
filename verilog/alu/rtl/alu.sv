ff6f7c377f09ed6f788c8e00415f2000882cfea0
unknown <michael.stangl2@edu.fh-joanneum.at>
2021-10-23 15:50:26 +0200

    linked source file to project

--- verilog/alu/rtl/alu.sv
+++ verilog/alu/rtl/alu.sv
@@ -10,7 +10,51 @@ module alu(
     input INC,
     
     // OUTPUTS
-    output [31:0] out,
+    output [31:0] ALU_out,
     output N,
     output Z);
+
+    logic [5:0] control_lines = {F0, F1, ENA, ENB, INVA, INC};
+    logic [31:0] result;
+    
+    always @(*)
+    begin
+        case(control_lines)
+        6'b011000:  //A
+            result = A;
+        6'b010100:  //B
+            result = B;
+        6'b011010:  //negate A
+            result = ~A;
+        6'b101100:  //negate B
+            result = ~B;
+        6'b111100:  //A+B
+            result = A+B;
+        6'b111101:  //A+B+1
+            result = A+B+1;
+        6'b111001:  //A+1
+            result = A+1;
+        6'b110101:  //B+1
+            result = B+1;
+        6'b111111:  //B-A
+            result = B-A;
+        6'b110110:  //B-1
+            result = B-1;
+        6'b111011:  //-A
+            result = -A;
+        6'b001100:  //A AND B
+            result = A&B;
+        6'b011100:  //A OR B
+            result = A|B;
+        6'b010000:  //0
+            result = 0;
+        6'b110001:  //1
+            result = 1;
+        6'b110010:  //-1
+            result = -1;
+        default: 
+            result = 0;
+        endcase
+    end
+
 endmodule
