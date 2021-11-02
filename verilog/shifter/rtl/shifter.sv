dff98697ff91ec81fd6710308a4df4ff4e19e0f3
MLang <michael.lang@edu.fh-joanneum.at>
2021-11-02 18:56:05 +0100

    Update shifter.sv

--- verilog/shifter/rtl/shifter.sv
+++ verilog/shifter/rtl/shifter.sv
@@ -2,26 +2,18 @@ module shifter(
     // INPUTS
     input logic [31:0] ALU_out,
     input logic [1:0] SET,
-  
     // OUTPUTS
-    output logic [31:0] Shift);
-    
+    output logic [31:0] Shift);   
     logic [31:0] operation;
-    
-    always@(*)
+    always_comb(*)
     begin
         case(SET)
-        2'b00:  operation = $signed(ALU_out);
-        
-        2'b01:  operation = $signed(ALU_out) << 8;
-        
-        2'b10:  operation = $signed(ALU_out) >>> 1;
-        
-        2'b11:  operation = $signed(ALU_out);
-        
+        2'b00:  operation = $signed(ALU_out);       
+        2'b01:  operation = $signed(ALU_out) << 8;        
+        2'b10:  operation = $signed(ALU_out) >>> 1; 
+        2'b11:  operation = $signed(ALU_out);       
         default: operation = 0;
         endcase
         Shift = operation;
-    end    
-    
+    end  
 endmodule
