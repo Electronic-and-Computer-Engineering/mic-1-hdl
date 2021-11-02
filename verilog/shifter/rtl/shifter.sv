56e8aebee07678d7c162c931b5847d4f41019ea1
MLang <michael.lang@edu.fh-joanneum.at>
2021-11-02 18:17:12 +0100

    shifter & testbench overhaul

--- verilog/shifter/rtl/shifter.sv
+++ verilog/shifter/rtl/shifter.sv
@@ -4,24 +4,24 @@ module shifter(
     input logic [1:0] SET,
   
     // OUTPUTS
-    output logic [31:0] Shifter_out);
+    output logic [31:0] Shift);
     
     logic [31:0] result;
     
-    always@(ALU_out)
+    always@(*)
     begin
         case(SET)
-        2'b00:  result = ALU_out;
+        2'b00:  result = $signed(ALU_out);
         
         2'b01:  result = $signed(ALU_out) << 8;
         
         2'b10:  result = $signed(ALU_out) >>> 1;
         
-        2'b11:  result = ALU_out;
+        2'b11:  result = $signed(ALU_out);
         
         default: result = 0;
         endcase
-        Shifter_out = result;
+        Shift = result;
     end    
     
 endmodule
--- verilog/shifter/tb/shifter_tb.sv
+++ verilog/shifter/tb/shifter_tb.sv
@@ -4,7 +4,7 @@ reg [31:0]in = 32'hAAAAAAAA;
 logic [1:0] set = 2'b00;
 reg [31:0]out = 32'h0;
 
-shifter DUT(.ALU_out(in), .SET(set), .Shifter_out(out));
+shifter DUT(.ALU_out(in), .SET(set), .Shift(out));
 
 always 
 begin
