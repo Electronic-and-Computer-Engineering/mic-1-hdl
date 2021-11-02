3b64cd9b9554d60dcebff296386bc70923460616
MLang <michael.lang@edu.fh-joanneum.at>
2021-11-02 18:06:17 +0100

    started with module programming

--- verilog/shifter/tb/shifter_tb.sv
+++ verilog/shifter/tb/shifter_tb.sv
@@ -2,21 +2,12 @@ module shifter_tb;
 
 reg [31:0]in = 32'hAAAAAAAA;
 logic [1:0] set = 2'b00;
-reg [31:0]out = 32'sh0;
+reg [31:0]out = 32'h0;
 
-//shifter DUT( .ALU_out(in), .set(set), .Shifter_out(out));
+shifter DUT(.ALU_out(in), .SET(set), .Shifter_out(out));
 
-initial begin
-    #50ns;
-    set = 2'b01;
-    out = $signed(in) << 8;
-    #100ns;
-    set = 2'b10;
-    out = $signed(in) >>> 1'b1;
-    #150ns;
-    out = in;
-    set = 2'b11;
-    #200ns;
-    $finish;
+always 
+begin
+    #250ns set++;
 end
 endmodule
