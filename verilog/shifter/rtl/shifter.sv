adfd8b45c4ce9561643f0293fb8fe3946f311c81
MLang <michael.lang@edu.fh-joanneum.at>
2021-11-02 18:24:31 +0100

    Shifter and Testbench Overhaul II

--- verilog/shifter/rtl/shifter.sv
+++ verilog/shifter/rtl/shifter.sv
@@ -6,22 +6,22 @@ module shifter(
     // OUTPUTS
     output logic [31:0] Shift);
     
-    logic [31:0] result;
+    logic [31:0] operation;
     
     always@(*)
     begin
         case(SET)
-        2'b00:  result = $signed(ALU_out);
+        2'b00:  operation = $signed(ALU_out);
         
-        2'b01:  result = $signed(ALU_out) << 8;
+        2'b01:  operation = $signed(ALU_out) << 8;
         
-        2'b10:  result = $signed(ALU_out) >>> 1;
+        2'b10:  operation = $signed(ALU_out) >>> 1;
         
-        2'b11:  result = $signed(ALU_out);
+        2'b11:  operation = $signed(ALU_out);
         
-        default: result = 0;
+        default: operation = 0;
         endcase
-        Shift = result;
+        Shift = operation;
     end    
     
 endmodule
