cfcbc67385b75ee6978bcca38e3bac33f6ffdcd5
unknown <zwitte46@gmail.com>
2021-11-03 22:05:08 +0100

    Testbench completed, tried to read a file

--- verilog/control-store/rtl/control_store.sv
+++ verilog/control-store/rtl/control_store.sv
@@ -3,16 +3,24 @@
 module control_store (
     input clk, wen, ren,
 
-    input [8:0] waddr, raddr
-    input [31:0] wdata,
-    output reg [31:0] rdata
+    input [8:0] waddr, raddr,
+    input [35:0] wdata,
+    output reg [35:0] rdata
     );
 
     reg [35:0] mem [0:511];
+    
+    initial begin
+        if ("some_values.mem" != 0) begin
+            $display("Loading memory file '%s' into bram_basic.", "some_values.mem");
+            $readmemh("some_values.mem", mem);
+        end
+    end
+    
     always @(posedge clk) begin
          if (wen)
             mem[waddr] <= wdata;
          if (ren)
-            rdata <= mem[raddr]
-
+            rdata <= mem[raddr];
+    end
 endmodule
--- verilog/control-store/tb/control_store_tb.sv
+++ verilog/control-store/tb/control_store_tb.sv
@@ -1,25 +1,44 @@
 `timescale 1 ns / 1 ps
 
-module testbench;
+module control_store_tb;
 
-reg clk = 1`b0;
+reg clk = 0, wen, ren;
 
-reg [8:0] waddr, raddr
-reg [31:0] wdata = 32'hAEDAADDA;
-reg [31:0] rdata = 32'sh0; // output register
+reg [8:0] waddr, raddr;
+reg [35:0] wdata = 'h0;
+wire [35:0] rdata; // output register
 
+always begin
+    #1
+    clk =! clk;
+end
+
+reg [35:0] mem [512];
+  
 initial begin
-    #50ns;
-    wdata = 2'b01;
-    rdata = wdata;
-    #100ns;
-    wdata = 2'b01;
-    rdata = wdata;
-    #150ns;
-    wdata = 2'b01;
-    rdata = wdata;
-    #200ns;
+    #10ns
+    wdata = 41;
+    waddr = 3;
+    wen = 1; #2 wen = 0;
+    #10ns
+    raddr = 3;
+    ren = 1; #2 ren = 0;
+    
+    assert (wdata == rdata) $display ("Memory Cell Check completed");
+      else $error("Memory Cell Check gone wrong");
+
+    #10
     $finish;
 end
     
+control_store control_store (
+.clk(clk),
+.wen(wen),
+.ren(ren),
+.waddr(waddr),
+.raddr(raddr),
+.wdata(wdata),
+.rdata(rdata)
+);    
+
 endmodule
