0899de11336748771cc5c4dbe3c6d3d258f21313
PetitProduction <plsinabell@gmail.com>
2021-11-03 22:24:13 +0100

    Updated main module and tb file

--- verilog/main-memory/rtl/main_memory.sv
+++ verilog/main-memory/rtl/main_memory.sv
@@ -2,18 +2,14 @@
 
 module main_memory (
     input clk, wen, ren,    
-    input [8:0] waddr, raddr,
-    input [8:0] wdata,
+    input [8:0] waddr, raddr, wdata,
     
     output reg [8:0] rdata
     );
 
-reg [8:0] mem [0:16000];
-always @(posedge clk)
-begin
-     if (wen)
-        mem[waddr] <= wdata;
-     if (ren)
-        rdata <= mem[raddr];
-end
+reg [8:0] test_memory [0:10];
+always @(posedge clk) begin
+    $readmemh("init_memory_test.mem", test_memory);
+       
+    end
 endmodule
--- verilog/main-memory/tb/tb_main_memory.sv
+++ verilog/main-memory/tb/tb_main_memory.sv
@@ -1,13 +1,36 @@
+`define assert(mem_value, written_value) \
+        if (mem_value !== written_value) begin \
+            $display("ASSERTION FAILED in %m: mem_value != written_value"); \
+            $finish; \
+        end
+
+
 module tb_main_memory ();
 
-logic clk = 0;
+logic clk, wen, ren = 0;
+logic [8:0] waddr, raddr, wdata;
 
-logic [8:0] waddr, raddr;
+integer i;
 
 always #5ns clk = ~clk; 
 
-initial
-begin
-
+reg [8:0] test_memory [0:10];   //16kB for simulation not possible because of size
+initial begin
+    #10ns     
+    wdata = 99;
+    waddr = 10;
+    
+    #10ns
+    test_memory[waddr] = wdata; 
+    
+    #10ns
+    `assert(test_memory[waddr], wdata)
+          
+    #10ns
+    for (i = 0; i <= 10; i = i + 1) begin
+        $display("%h", test_memory[i]);
+    end
+    
+    
 end
 endmodule 
