891454b007473d041a99e0923c18b55748df4325
MLang <michael.lang@edu.fh-joanneum.at>
2021-11-06 19:35:29 +0100

    Setup Project & Files

---
+++ verilog/decoder/rtl/decoder.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06.11.2021 19:34:48
+// Design Name: 
+// Module Name: decoder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module decoder(
+
+    );
+endmodule
