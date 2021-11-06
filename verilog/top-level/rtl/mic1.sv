c2c127acc99ede741927043a7c489eb6da95b5c3
Leo Moser <leomoser99@gmail.com>
2021-11-06 18:06:14 +0100

    Add small microcode testbench

--- verilog/top-level/rtl/mic1.sv
+++ verilog/top-level/rtl/mic1.sv
@@ -30,7 +30,7 @@ module mic1 (
     reg [8:0]  MPC = 0;
     
     // C "bus"
-    wire [31:0] C = 0;
+    wire [31:0] C;
     
     // B "bus"
     reg [31:0] B = 0;
@@ -60,33 +60,49 @@ module mic1 (
     assign jump_ctrl    = MIR[26:24];
     assign next_address = MIR[35:27];
     
-    /*ALU ALU (
-        .A(H),
-        .B(B),
-        .out(ALU_out),
+    alu alu (
+        .F0     (ALU_ctrl[5]),
+        .F1     (ALU_ctrl[4]),
+        .ENA    (ALU_ctrl[3]),
+        .ENB    (ALU_ctrl[2]),
+        .INVA   (ALU_ctrl[1]),
+        .INC    (ALU_ctrl[0]),
+        .A      (H),
+        .B      (B),
         
-        .control(ALU_ctrl),
-        
-        .N(N),
-        .Z(Z)
-    );*/
+        .ALU_out(ALU_out),
+        .Z      (Z),
+        .N      (N)
+    );
     
-    /*shifter shifter(
-        .in(ALU_out),
-        .out(C),
+    shifter shifter (
+        .ALU_out    (ALU_out), 
+        .SET        (shifter_ctrl),
         
-        .control(shifter_ctrl)
-    );*/
+        .Shift      (C)
+    );
     
     // Write to B bus
-    // TODO load MIR?
+    // TODO load MIR from memory with address MPC
     always_ff @(negedge clk) begin
         if (!resetn) begin
             
         end else begin
-
+            case (B_select)
+                4'd0: B = MDR;
+                4'd1: B = PC;
+                4'd2: B = $signed(MBR);
+                4'd3: B = MBR; // MBRU
+                4'd4: B = SP;
+                4'd5: B = LV;
+                4'd6: B = CPP;
+                4'd7: B = TOS;
+                4'd8: B = OPC;
+                default: B = 'X;
+            endcase
         end
     end
+
     
     // Write from C bus into registers
     // Set N and Z
@@ -98,11 +114,59 @@ module mic1 (
             N_ff <= N;
             Z_ff <= Z;
 
-            if (C_select & 1<<0) begin
-            
+            if (C_select & 9'b000000001) begin
+                MAR <= C;
+            end
+            if (C_select & 9'b000000010) begin
+                MDR <= C;
+            end
+            if (C_select & 9'b000000100) begin
+                PC <= C;
+            end
+            if (C_select & 9'b000001000) begin
+                SP <= C;
+            end
+            if (C_select & 9'b000010000) begin
+                LV <= C;
+            end
+            if (C_select & 9'b000100000) begin
+                CPP <= C;
+            end
+            if (C_select & 9'b001000000) begin
+                TOS <= C;
+            end
+            if (C_select & 9'b010000000) begin
+                OPC <= C;
+            end
+            if (C_select & 9'b100000000) begin
+                H <= C;
             end
             
+            
         end
+    end
+    
+    always_comb begin
+        // JMPC
+        if (jump_ctrl[2]) begin
+            MPC = next_address | MBR;
+        end else begin
+            MPC = next_address | ((( jump_ctrl[0] && Z ) || ( jump_ctrl[1] && N )) << 9);
+        end
+    end
+    
+    reg [35:0] mem [0:511];
+
+    initial begin
+        $display("Loading memory file into microprogram.");
+        $readmemb("microcode.mem", mem, 0, 2);
+    end
+
+    reg ren = 1;
+
+    always @(posedge clk) begin
+         if (ren && resetn)
+            MIR <= mem[MPC];
     end
 
 endmodule
