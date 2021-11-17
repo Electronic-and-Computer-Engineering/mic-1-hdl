8023af24d98fbec8a0d3bace7e41eafe3480163f
MLang <michael.lang@edu.fh-joanneum.at>
2021-11-17 17:04:15 +0100

    Decoder Implementation

--- verilog/decoder/tb/decoder_tb.sv
+++ verilog/decoder/tb/decoder_tb.sv
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 06.11.2021 19:34:48
-// Design Name: 
-// Module Name: decoder_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module decoder_tb(
 
     );
