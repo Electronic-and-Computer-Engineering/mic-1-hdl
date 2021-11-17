a4dca1f297ec38f844d7212c15ff972d61cc08bf
PetitProduction <plsinabell@gmail.com>
2021-11-18 00:23:28 +0100

    updated main_memory module + test file

--- verilog/main-memory/rtl/main_memory.sv
+++ verilog/main-memory/rtl/main_memory.sv
@@ -4,24 +4,31 @@
 // B - read port
 
 module main_memory (
-    input clk, wen_A, ren_A, ren_B,   
-    input [8:0] addr_A, addr_B, wdata_A,
-    
+    input logic clk, wen_A, ren_A, ren_B,   
+    input logic [8:0] addr_A, addr_B, wdata_A,
+        
     output reg [8:0] rdata_A,
 	output reg [8:0] rdata_B
     );
-
-reg [8:0] test_memory [0:10];
+   
+   reg [8:0] test_memory [0:9];
 
 initial $readmemh("init_memory_test.mem", test_memory);
 
-always @(posedge clk) begin	// read/write
-    
+
+// PORT A - Read/Write
+always @(posedge clk) begin
+    if (wen_A)
+        test_memory[addr_A] <= wdata_A;
+    if (ren_A)
+        rdata_A <= test_memory[addr_A];
        
     end
 
-always @(posedge clk) begin	// read
-    
+// PORT B - Read
+always @(posedge clk) begin
+    if (ren_B)
+        rdata_B <= test_memory[addr_B];
        
 	end
 endmodule
--- verilog/main-memory/tb/tb_main_memory.sv
+++ verilog/main-memory/tb/tb_main_memory.sv
@@ -1,35 +1,52 @@
-`define mem_assert(mem_value, written_value) \
-        if (mem_value !== written_value) begin \
-            $display("ASSERTION FAILED in %m: mem_value != written_value"); \
-            $finish; \
-        end
-
+`timescale 1 ns / 1 ps
 
 module tb_main_memory ();
 
-logic clk, wen, ren = 0;
-logic [8:0] waddr, raddr, wdata;
+logic clk, wen, wen_A, ren_A, ren_B = 0;
+logic [8:0] addr_A, addr_B, wdata_A;
+
+reg [8:0] tb_memory [0:9];
+reg [8:0] rdata_A;
+reg [8:0] rdata_B;
 
 integer i;
 
 always #5ns clk = ~clk; 
 
-main_memory main_memory(blabla);
+main_memory main_memory(.clk(clk),
+                        .wen_A(wen_A),
+                        .ren_A(ren_A),
+                        .ren_B(ren_B),
+                        .addr_A(addr_A),
+                        .addr_B(addr_B),
+                        .wdata_A(wdata_A),
+                        .rdata_A(rdata_A),
+                        .rdata_B(rdata_B)
+                        );
 
-initial begin
-    #10ns     
-    wdata = 99;
-    waddr = 10;
-    
-       
+initial 
+begin
+    // read first 5 memory cells
     #10ns
-    `mem_assert(test_memory[waddr], wdata)
-          
-    #10ns
-    for (i = 0; i <= 10; i = i + 1) begin
-        $display("cell [%d] = %h", i, main_memory.test_memory[i]);
+    for (i = 0; i < 5; i++) begin
+        #10ns
+        addr_A = i;
+        ren_A = 1;
+        #10ns
+        tb_memory[i] <= rdata_A;
+        #10ns
+        ren_A = 0;        
+    end
+              
+      
+    // checking if init_memory was loaded right
+    for (i = 0; i < 9; i = i + 1) begin
+        assert(main_memory.test_memory[i]== tb_memory[i]) $display ("index=%2.d: PASS", i);
+            else $error("index=%2.d: FAIL", i);
     end
     
     
 end
 endmodule 
+
+
