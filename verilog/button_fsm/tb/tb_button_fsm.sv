c0e61f2ef7006edb64e0e61e5ffe56b1d86ee980
PetitProduction <plsinabell@gmail.com>
2021-12-08 19:03:10 +0100

    changed tb

--- verilog/button_fsm/tb/tb_button_fsm.sv
+++ verilog/button_fsm/tb/tb_button_fsm.sv
@@ -8,7 +8,9 @@ logic tb_resetn = 0;
 logic [4:0] tb_button;
 
 // outputs
-logic [5:0] tb_led;
+logic tb_led_start_stop;
+logic tb_led_step;
+logic [3:0] tb_led_run;
 
 // set clock
 always #1ns tb_clk = ~tb_clk;
@@ -17,11 +19,14 @@ always #1ns tb_clk = ~tb_clk;
 button_fsm button_fsm(.clk(tb_clk),
                       .resetn(tb_resetn),
                       .button(tb_button),
-                      .led(tb_led));
+                      .led_start_stop(tb_led_start_stop),
+                      .led_step(tb_led_step),
+                      .led_run(tb_led_run));
 
 initial
 begin
-
+    #10ns
+    tb_resetn = 0;
     #10ns
     tb_button[0] = 1;
     #10ns
